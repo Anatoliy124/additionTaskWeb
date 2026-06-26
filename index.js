e8f3ed095e1037e36ffc538962c5ed1fc9e15858
BOOMS <anatoliyrodionov27@gmail.com>
2026-06-26 12:41:52 +0300

    Fix zipper endpt

--- index.js
+++ index.js
@@ -15,18 +15,30 @@ app.get('/login', (req, res) => {
   res.send('anatoliy409453');
 });
 
-app.post('/zipper', upload.single('file'), (req, res) => {
-  if (!req.file) {
+app.post('/zipper', (req, res) => {
+  let buffer;
+
+  if (req.file && req.file.buffer) {
+    buffer = req.file.buffer;
+  } else if (req.files && req.files[0]) {
+    buffer = req.files[0].buffer;
+  } else if (req.body && Buffer.isBuffer(req.body)) {
+    buffer = req.body;
+  }
+
+  if (!buffer) {
     return res.status(400).send('No file');
   }
 
-  gzip(req.file.buffer, (err, data) => {
-    if (err) {
-      return res.status(500).send('Compression error');
-    }
+  gzip(buffer, (err, data) => {
+    if (err) return res.status(500).send('Compression error');
 
-    res.set('Content-Type', 'application/gzip');
-    res.send(data);
+    res.writeHead(200, {
+      'Content-Type': 'application/gzip',
+      'Content-Length': data.length
+    });
+
+    res.end(data);
   });
 });
 
