9bed4f486993c53a6163e71a74785b8e3a824ddc
BOOMS <anatoliyrodionov27@gmail.com>
2026-06-26 12:00:07 +0300

    accept any file field

--- index.js
+++ index.js
@@ -5,36 +5,22 @@ import { gzip } from 'zlib';
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
-app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-app.get('/login', (req, res) => {
-  res.type('text/plain; charset=UTF-8');
-  res.send('anatoliy409453');
-});
-
-app.post('/zipper', upload.single('file'), (req, res) => {
-  if (!req.file) {
+app.post('/zipper', upload.any(), (req, res) => {
+  if (!req.files || req.files.length === 0) {
     return res.status(400).send('No file');
   }
 
-  gzip(req.file.buffer, (err, data) => {
+  const file = req.files[0];
+
+  gzip(file.buffer, (err, data) => {
     if (err) {
       return res.status(500).send('Compression error');
     }
 
-    res.set('Content-Type', 'application/gzip');
-    res.set(
-      'Content-Disposition',
-      `attachment; filename="${req.file.originalname}.gz"`
-    );
-
+    res.type('application/gzip');
     res.send(data);
   });
 });
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
